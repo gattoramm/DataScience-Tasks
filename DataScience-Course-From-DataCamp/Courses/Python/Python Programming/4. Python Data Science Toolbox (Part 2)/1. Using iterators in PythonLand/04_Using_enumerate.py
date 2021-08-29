b876e9302a2cc3ac2c5987fc6a25a0eda72023e9
RAMIL ARSAEV <gattoramm@gmail.com>
2021-08-29 13:38:50 +0300

    p_4_1

---
+++ DataScience-Course-From-DataCamp/Courses/Python/Python Programming/4. Python Data Science Toolbox (Part 2)/1. Using iterators in PythonLand/04_Using_enumerate.py
@@ -0,0 +1,28 @@
+"""
+Using enumerate
+
+In this exercise, you are given a list of strings mutants and you will
+practice using enumerate() on it by printing out a list of tuples and
+unpacking the tuples using a for loop.
+"""
+# Create a list of strings: mutants
+mutants = ['charles xavier',
+            'bobby drake',
+            'kurt wagner',
+            'max eisenhardt',
+            'kitty pryde']
+
+# Create a list of tuples: mutant_list
+mutant_list = list(enumerate(mutants))
+
+# Print the list of tuples
+print(mutant_list)
+
+# Unpack and print the tuple pairs
+for index1, value1 in enumerate(mutants):
+    print(index1, value1)
+
+# Change the start index
+for index2, value2 in enumerate(mutants, start = 1):
+    print(index2, value2)
+
